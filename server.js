2317c9e1878f9c0e2b98ee1d71b69084e58e0ca9
Raul <simion.iles@yahoo.com>
2023-11-01 18:32:06 +0200

    update database

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const image=require('./controllers/image.js')
 const db=knex({
 	client: 'pg',
     connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-silhouetted-38706',
     port : 5432,
     user : 'postgres',
     password : 'Steauabucuresti9',
